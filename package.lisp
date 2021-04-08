1323baa3899648c4336765e5c6d58b5309d25a44
Matthew Veety <mveety@gmail.com>
2021-04-08 01:09:34 -0400

    made package a bit denser

--- package.lisp
+++ package.lisp
@@ -7,42 +7,17 @@
   (:use :cl :dlist :alexandria :split-sequence)
   (:export
    ;; message passing bits
-   #:port
-   #:msg
-   #:sendmsg
-   #:getreply
-   #:message
-   #:reply
-   #:waitformsg
+   #:port #:msg #:sendmsg #:getreply
+   #:message #:reply #:waitformsg
    ;; message passing event loop
-   #:add-command
-   #:command-exists
-   #:find-command
-   #:del-command
-   #:run-command
-   #:run-table
-   #:sendcmd
-   #:get-command-list
-   #:defcommand
-   #:add-template-to-cmd-table
+   #:add-command #:command-exists #:find-command #:del-command
+   #:run-command #:run-table #:sendcmd #:get-command-list
+   #:defcommand #:add-template-to-cmd-table
    ;; textbuf
-   #:set-dot
-   #:get-dot
-   #:insert-line
-   #:remove-line
-   #:insert-char
-   #:remove-char
-   #:get-char
-   #:set-char
-   #:get-line
-   #:line-length
+   #:set-dot #:get-dot #:insert-line #:remove-line
+   #:insert-char #:remove-char #:get-char #:set-char
+   #:get-line #:line-length
    ;; textbuf-tools
-   #:make-textbuf
-   #:tbuf-to-list
-   #:get-n-lines
-   #:line-list-to-cs
-   #:line-list-to-strings
-   #:line-list-to-chars
-   #:merge-lines
-   #:split-line
+   #:make-textbuf #:tbuf-to-list #:get-n-lines #:line-list-to-cs
+   #:line-list-to-strings #:line-list-to-chars #:merge-lines #:split-line
    ))
